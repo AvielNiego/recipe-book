34858c4ede995b3c3b83dc6805ac66b62ea04000
Aviel <avielniego@gmail.com>
2017-01-20 04:42:56 +0200

    init

---
+++ out-tsc/app/recipes/recipe.js
@@ -0,0 +1,10 @@
+export var Recipe = (function () {
+    function Recipe(name, description, imagePath, ingredients) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+        this.ingredients = ingredients;
+    }
+    return Recipe;
+}());
+//# sourceMappingURL=C:/Develop/AngularPractice/recipe-book/src/app/recipes/recipe.js.map
---
+++ out-tsc/environments/environment.prod.js
@@ -0,0 +1,4 @@
+export var environment = {
+    production: true
+};
+//# sourceMappingURL=C:/Develop/AngularPractice/recipe-book/src/environments/environment.prod.js.map
