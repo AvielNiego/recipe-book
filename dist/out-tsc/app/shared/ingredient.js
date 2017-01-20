85aa5b2de19b87461e9c61ccb0b988960ebb80a8
Aviel <avielniego@gmail.com>
2017-01-20 05:09:24 +0200

    delete dist

--- dist/out-tsc/app/shared/ingredient.js
+++
@@ -1,8 +0,0 @@
-export var Ingredient = (function () {
-    function Ingredient(name, amount) {
-        this.name = name;
-        this.amount = amount;
-    }
-    return Ingredient;
-}());
-//# sourceMappingURL=C:/Develop/AngularPractice/recipe-book/src/app/shared/ingredient.js.map
--- dist/out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=C:/Develop/AngularPractice/recipe-book/src/environments/environment.prod.js.map
